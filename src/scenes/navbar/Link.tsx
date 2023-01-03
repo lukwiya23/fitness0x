84e769a895d16d3ad137270016748f5ce0e47871
lukwiya23 <luk23bonnie8@gmail.com>
2023-01-03 12:27:19 +0300

    nav

---
+++ src/scenes/navbar/Link.tsx
@@ -0,0 +1,25 @@
+import AnchorLink from 'react-anchor-link-smooth-scroll'
+import {useState} from 'react'
+type Props = {
+    page: String;
+    selectedPage:String;
+    setSelectedPage:(value: String) => void;
+}
+
+const Link = ({page,selectedPage,setSelectedPage}: Props) => {
+const lowerCasePage = page.toLowerCase().replace(/ /g, "")
+
+  return (
+    <AnchorLink
+    className={`${selectedPage === lowerCasePage ? "text-primary-500" : ""}
+      transition duration-500 hover:text-primary-300
+    `}
+    href={`#${lowerCasePage}`}
+    onClick={()=> setSelectedPage(lowerCasePage)}
+  >
+        {page}
+    </AnchorLink>
+  )
+}
+
+export default Link
