51d3c538af42cfa0e45e20897fbfa756fb1a4f7a
lukwiya23 <luk23bonnie8@gmail.com>
2023-01-04 09:31:09 +0300

    navbar done

---
+++ src/scenes/home/index.tsx
@@ -0,0 +1,16 @@
+import { SelectedPage } from "@/shared/types";
+import React from "react";
+
+type Props = {
+   setSelectedPage: (value: SelectedPage) => void
+};
+
+const Home = (props: Props) => {
+  return(
+  <div className="">
+    
+    </div>
+  )
+};
+
+export default Home;
